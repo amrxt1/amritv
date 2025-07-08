f4e4fb1b6ca67d09aa188fdaea18e0c48be58e3a
amrxt1 <115386946+amrxt1@users.noreply.github.com>
2025-07-07 18:51:00 -0700

    Add filler text

--- src/app/page.js
+++ src/app/page.js
@@ -1,20 +1,27 @@
-"use client"
-import {motion} from "motion/react";
+"use client";
+import { motion } from "motion/react";
 
 export default function Home() {
     return (
-        <motion.div 
-        animate={{opacity:0}} 
-        transition={{duration: 1.1, ease: "easeInOut"}}  
-        className="w-full h-[100vh] text-5xl 
-        font-mono fixed flex justify-center items-center bg-gray-300">
-            <motion.p
-            animate={{scale:0.96}}
-            transition={{duration:1.1}}
-            className="hover:text-blue-400">
-                <span className="text-blue-400">amritv</span>
-                <span className="text-black">.dev</span>
-            </motion.p>
-        </motion.div>
+        <>
+            <motion.div
+                animate={{ opacity: 0 }}
+                transition={{ duration: 1.1, ease: "easeInOut" }}
+                className="w-full h-[100vh] text-5xl 
+        font-mono fixed flex justify-center items-center bg-gray-300"
+            >
+                <motion.p
+                    animate={{ scale: 0.96 }}
+                    transition={{ duration: 1.1 }}
+                    className="hover:text-blue-400"
+                >
+                    <span className="text-blue-400">amritv</span>
+                    <span className="text-black">.dev</span>
+                </motion.p>
+            </motion.div>
+            <div class="flex justify-center items-center font-mono">
+                amritv.dev
+            </div>
+        </>
     );
 }
