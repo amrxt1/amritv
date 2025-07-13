c0d26e439f9f8337aacc25447e27595320ae50db
amrxt1 <115386946+amrxt1@users.noreply.github.com>
2025-07-12 17:44:36 -0700

    Messing with framer-motion

--- src/app/page.js
+++ src/app/page.js
@@ -2,28 +2,35 @@
 import { motion } from "motion/react";
 
 export default function Home() {
-    return (
-        <>  
-            <motion.div
-                animate={{ opacity: 0, display: "none"}}
-                transition={{ duration: 1.1, ease: "easeInOut" }}
-                className="w-full h-[100vh] text-5xl 
-                font-mono fixed flex justify-center items-center bg-gray-300 z-10"
-                >
-                <motion.p
-                    animate={{ scale: 0.99 }}
-                    transition={{ duration: 0.7 }}
-                    >
-                    <span className="text-blue-500">amritv</span>
-                    <span className="text-black">.dev</span>
-                </motion.p>
-            </motion.div>
-            <div className="flex justify-center items-center font-mono h-[100vh] text-5xl">
-                <p className="hover:text-blue-400 text-white">
-                    <span className="text-blue-400">amritv</span>
-                    <span className="">.dev</span>
-                </p>
-            </div>
-        </>
-    );
+  return (
+    <>
+      <motion.div
+        initial={{ opacity: 1, x: 0 }}
+        animate={{ opacity: 0, x: 10, display: "none" }}
+        transition={{ duration: 1, ease: "easeInOut" }}
+        className="w-full text-5xl 
+                font-mono fixed flex justify-center z-10"
+      >
+        <p className="py-4">
+          <span className="text-blue-400">amritv</span>
+          <span className="text-white">.dev</span>
+        </p>
+      </motion.div>
+      <div className="flex justify-center items-center font-mono text-5xl">
+        <p className="hover:text-blue-400 text-white py-4">
+          <span className="text-blue-400">amritv</span>
+          <span className="">.dev</span>
+        </p>
+      </div>
+
+      <div className="">
+        <motion.div
+          initial={{ scaleX: 0 }}
+          animate={{ scaleX: 1 }}
+          transition={{ duration: 1, ease: "easeInOut" }}
+          className="h-1 w-full bg-blue-300 origin-left"
+        />
+      </div>
+    </>
+  );
 }
