2b62811be94390e7d06ebc223f5647cb2f31fc58
amrxt1 <115386946+amrxt1@users.noreply.github.com>
2025-08-24 13:26:22 -0700

    Messing with NextJs routing

---
+++ src/app/contact/page.js
@@ -0,0 +1,14 @@
+"use client";
+import { motion } from "motion/react";
+
+export default function Home() {
+  return (
+    <div className="px-32 mt-4">
+      <form className="grid bg-surface">
+        <input placeholder="Name" />
+        <input placeholder="Email" />
+        <textarea placeholder="Message" rows={3} className="resize-none" />
+      </form>
+    </div>
+  );
+}
--- src/app/page.js
+++ src/app/page.js
@@ -2,35 +2,35 @@
 import { motion } from "motion/react";
 
 export default function Home() {
-  return (
-    <>
-      <motion.div
-        initial={{ opacity: 1, x: 0 }}
-        animate={{ opacity: 0, x: 10, display: "none" }}
-        transition={{ duration: 1, ease: "easeInOut" }}
-        className="w-full text-5xl 
+    return (
+        <>
+            <motion.div
+                initial={{ opacity: 1, x: 0 }}
+                animate={{ opacity: 0, x: 10, display: "none" }}
+                transition={{ duration: 1, ease: "easeInOut" }}
+                className="w-full text-5xl
                 font-mono fixed flex justify-center z-10"
-      >
-        <p className="py-4">
-          <span className="text-blue-400">amritv</span>
-          <span className="text-white">.dev</span>
-        </p>
-      </motion.div>
-      <div className="flex justify-center items-center font-mono text-5xl">
-        <p className="hover:text-blue-400 text-white py-4">
-          <span className="text-blue-400">amritv</span>
-          <span className="">.dev</span>
-        </p>
-      </div>
+            >
+                <p className="py-4">
+                    <span className="text-blue-400">amritv</span>
+                    <span className="text-white">.dev</span>
+                </p>
+            </motion.div>
+            <div className="flex justify-center items-center font-mono text-5xl">
+                <p className="hover:text-blue-400 text-white py-4">
+                    <span className="text-blue-400">amritv</span>
+                    <span className="">.dev</span>
+                </p>
+            </div>
 
-      <div className="">
-        <motion.div
-          initial={{ scaleX: 0 }}
-          animate={{ scaleX: 1 }}
-          transition={{ duration: 1, ease: "easeInOut" }}
-          className="h-1 w-full bg-blue-300 origin-left"
-        />
-      </div>
-    </>
-  );
+            <div className="">
+                <motion.div
+                    initial={{ scaleX: 0 }}
+                    animate={{ scaleX: 1 }}
+                    transition={{ duration: 1, ease: "easeInOut" }}
+                    className="h-1 w-full bg-blue-300 origin-left"
+                />
+            </div>
+        </>
+    );
 }
