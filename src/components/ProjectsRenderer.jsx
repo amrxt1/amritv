b0cd68a0e83e5d8614d1f30c6d1d591294180dd4
amrxt1 <115386946+amrxt1@users.noreply.github.com>
2025-08-24 19:40:03 -0700

    Add projects and their filter

--- src/components/ProjectsRenderer.jsx
+++ src/components/ProjectsRenderer.jsx
@@ -2,16 +2,98 @@
 
 import Container from "@/components/Container";
 import { motion } from "motion/react";
+import { useState } from "react";
 
 const projects = [
   {
-    title: "lolcat",
+    title: "armV7",
     desc: "",
     img: "",
-    techUsed: "",
+    techUsed: ["verilog"],
     features: "",
     details: "",
     id: 0,
+  },
+  {
+    title: "CCHive-Backend",
+    desc: "",
+    img: "",
+    techUsed: ["ruby"],
+    features: "",
+    details: "",
+    id: 1,
+  },
+  {
+    title: "CCHive-Frontend",
+    desc: "",
+    img: "",
+    techUsed: ["js"],
+    features: "",
+    details: "",
+    id: 2,
+  },
+  {
+    title: "Assembler",
+    desc: "",
+    img: "",
+    techUsed: ["asm"],
+    features: "",
+    details: "",
+    id: 3,
+  },
+  {
+    title: "Fire Hall Events",
+    desc: "",
+    img: "",
+    techUsed: ["js"],
+    features: "",
+    details: "",
+    id: 4,
+  },
+  {
+    title: "QuickMove Solutions",
+    desc: "",
+    img: "",
+    techUsed: ["js"],
+    features: "",
+    details: "",
+    id: 5,
+  },
+  {
+    title: "shiftBOT",
+    desc: "",
+    img: "",
+    techUsed: ["python"],
+    features: "",
+    details: "",
+    id: 6,
+  },
+  {
+    title: "Applications BOT",
+    desc: "",
+    img: "",
+    techUsed: ["python"],
+    features: "",
+    details: "",
+    id: 7,
+  },
+  {
+    title: "Project: Facebook",
+    desc: "",
+    img: "",
+    techUsed: ["ruby"],
+    features: "",
+    details: "",
+    id: 8,
+  },
+  {
+    title: "CS-120",
+    desc: "",
+    img: "",
+    techUsed: ["python"],
+    features: "",
+    details: "",
+    id: 9,
   },
 ];
 
@@ -32,16 +114,35 @@ const ALL_TECH = [
 ];
 
 const ProjectsRenderer = () => {
+  const [selectTech, setTech] = useState("all");
+
+  const filteredProjects =
+    selectTech === "all"
+      ? projects
+      : projects.filter((p) => p.techUsed.includes(selectTech));
+
   return (
     <Container>
-      <div className="flex gap-1">
-        {ALL_TECH && ALL_TECH.map((t) => <p key={t}>{t}</p>)}
+      <div className="flex w-full justify-around gap-1">
+        {ALL_TECH &&
+          ALL_TECH.map((t) => (
+            <button
+              key={t}
+              onClick={(e) => {
+                e.preventDefault();
+                setTech(t);
+              }}
+            >
+              {t}
+            </button>
+          ))}
       </div>
-
-      <h1>Projects</h1>
-      {projects.map((p) => {
-        return <ProjectCard project={p} key={p.id} />;
-      })}
+      <div className="bg-surface mt-4 rounded-lg p-4">
+        <h1 className="text-primary font-bold">Projects</h1>
+        {filteredProjects.map((p) => {
+          return <ProjectCard key={p.id} project={p} />;
+        })}
+      </div>
     </Container>
   );
 };
